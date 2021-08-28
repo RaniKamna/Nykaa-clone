cf020d278aeba8153fe6d23b57c70da7d05a32fe
Sagar Kurewar <sagarkurewar@gmail.com>
2021-08-28 12:46:03 +0530

    price_detail added to local storage

--- script/L_cart.js
+++ script/L_cart.js
@@ -42,26 +42,31 @@ let total_div = document.getElementById('total_d');
 let div_b_total = document.getElementById('div_b_total');
 let p_b_total = document.getElementById('p_b_total');
 p_b_total.innerText = `₹${total}`
+let T = p_b_total.innerText = `₹${total}`
 div_b_total.append(p_b_total);
 
 let div_b_discount = document.getElementById('div_b_discount');
 let p_b_discount = document.getElementById('p_b_discount');
 p_b_discount.innerText = `₹${0}`
+let D = p_b_discount.innerText = `₹${0}`
 div_b_discount.append(p_b_discount);
 
 let div_s_total = document.getElementById('div_s_total');
 let p_s_total = document.getElementById('p_s_total');
 p_s_total.innerText = `₹${total}`
+let S_T = p_s_total.innerText = `₹${total}`
 div_s_total.append(p_s_total);
 
 let shiping_div = document.getElementById('shiping_div');
 let p_shipping = document.getElementById('p_shipping');
 p_shipping.innerText = `FREE`;
+let S = p_shipping.innerText = `FREE`;
 shiping_div.append(p_shipping);
 
 let g_total_div = document.getElementById('g_total_div');
 let p_g_total = document.getElementById('p_g_total');
 p_g_total.innerText = `₹${total}`
+let G_T = p_g_total.innerText = `₹${total}`
 g_total_div.append(p_g_total);
 
 
@@ -82,8 +87,11 @@ function discount_30() {
     console.log(p_code);
 }
 
-function chckOut() {
-    `<a ${href=success.html}></a>`
-}
+let price_detail = [T, D, S_T, S, G_T]
+console.log(price_detail);
+
+localStorage.setItem("Price_Detail", JSON.stringify(price_detail))
+
+
 
 
